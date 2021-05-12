f398a47c88def46769c53d238018f66e52e10f73
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-12 12:41:32 +0530

    Create MulTwoNumbers.java

---
+++ MulTwoNumbers.java
@@ -0,0 +1,10 @@
+public class MulTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, res;
+      res = num2 * num1;
+
+      System.out.println("Diff of these numbers: " +res);
+   }
+}
